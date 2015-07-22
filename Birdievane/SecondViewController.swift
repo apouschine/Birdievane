817fa72ea33910561fc970f9cd3960d94f434aa1
Alex Pouschine <Alex@Alexs-MacBook-Pro.local>
2015-07-22 17:12:53 -0400

    Initial Commit

---
+++ Birdievane/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Birdievane
+//
+//  Created by Alex Pouschine on 7/22/15.
+//  Copyright (c) 2015 Alex Pouschine. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
